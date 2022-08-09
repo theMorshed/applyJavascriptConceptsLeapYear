5e8a909f4e2176b9948498d8c0d75b438ca3c339
theMorshed <morshedfive@gmail.com>
2022-08-09 09:44:19 +0600

    leap year problem solved

---
+++ leapYear.js
@@ -0,0 +1,15 @@
+/*
+১. leapYear() নামে ফাংশন লিখো এবং নেক্সট ইয়ার অর্থাৎ ২০২৩ কি leap year নাকি সেটা চেক করো। Leap year হলে ফাংশন true রিটার্ন করবে আর leap year না হলে false রিটার্ন করবে।
+    if year divisible by 4 this is leap year
+    and if divisible by 100 then must be divisible by 400
+*/
+
+function leapYear(year) {
+    // check if the year is leap year
+    if (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)) {
+        return true;
+    }
+    return false;
+}
+
+console.log(leapYear(2020));
